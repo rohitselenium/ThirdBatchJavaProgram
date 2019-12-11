38d30e92013ff17520fdd0113fbcc854bfd20926
rohranja <rohranja@WKWIN1700222.global.publicisgroupe.net>
2019-12-11 06:35:33 +0530

    DataType

---
+++ ThirdBatchJavaProgram/src/com/javaprogram/DataType.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.javaprogram;
+
+/**
+ * @author rohranja
+ *
+ */
+public class DataType {
+
+	/**
+	 * @param args
+	 */
+	static int i;
+	static byte b;
+	static char c;
+	static boolean t;
+	static long l;
+	static float f=0.0f;
+	static double d=0.0d;
+	static short s;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("***************************Size of primitive Data type **************************");
+		System.out.println("Size of Byte Data Type is :: " + Byte.SIZE/8 + " byte");
+		
+		System.out.println("Size of Short Data Type is :: " + Short.SIZE/8 + " byte");
+		
+		System.out.println("Size of Integer Data Type is :: " + Integer.SIZE/8 + " byte");
+		
+		System.out.println("Size of Long Data Type is :: " + Long.SIZE/8 + " byte");
+		
+		System.out.println("Size of Float Data Type is :: " + Float.SIZE/8 + " byte");
+		
+		System.out.println("Size of Double Data Type is :: " + Double.SIZE/8 + " byte");
+		
+		System.out.println("Size of Charecter Data Type is :: " + Character.SIZE/8 + " byte");
+	
+		System.out.println("***************************Default value of primitive Data type **************************");
+		System.out.println("Default value of Byte Data Type is :: " + b);
+		
+		System.out.println("Default value of Short Data Type is :: " + s);
+		
+		System.out.println("Default value of Integer Data Type is :: " + i);
+		
+		System.out.println("Default value of Long Data Type is :: " + l);
+		
+		System.out.println("Default value of Float Data Type is :: " + f);
+		
+		System.out.println("Default value of Double Data Type is :: " + d);
+		
+		System.out.println("Default value of Charecter Data Type is :: " + c);
+		
+		System.out.println("Default value of Boolean Data Type is :: " + t);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
